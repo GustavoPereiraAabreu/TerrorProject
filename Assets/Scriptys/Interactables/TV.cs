1af7b322424e069ac5e3251334f59bc7c4b77b08
GUSTAVO PEREIRA ABREU <gustavop.4breu@gmail.com>
2026-05-19 11:35:56 -0300

    MAISAjustes

---
+++ Assets/Scriptys/Interactables/TV.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.Video;
+
+public class TV : MonoBehaviour, IInteractable
+{
+    private VideoPlayer _videoPlayer;
+    private GameObject _videoContainer;
+
+    public void Interact()
+    {
+        if (_videoContainer.isPlaying)
+        {
+            _videoPlayer.Stop();
+            _videoContainer.SetActive(false);
+        }
+        else
+        {
+            _videoContainer.SetActive(true);
+            _videoPlayer.Play();
+        }
+
+    }
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+       _videoPlayer = GetComponent<VideoPlayer>();
+       _videoContainer = _videoPlayer.gameObject;
+
+    }
+
+}
