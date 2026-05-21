dea4def14687031303738baca20a579542df8266
GUSTAVO PEREIRA ABREU <gustavop.4breu@gmail.com>
2026-05-21 15:34:00 -0300

    MaisAjustes

---
+++ Assets/Scriptys/Collectables/Battery.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Battery : MonoBehaviour, ICollectable
+{
+    public void Collect()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void HideOutline()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void ShowOutline()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
