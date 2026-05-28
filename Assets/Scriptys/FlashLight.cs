60e78b662b7077bc7f999f6e81d354f1bfa4657f
GUSTAVO PEREIRA ABREU <gustavop.4breu@gmail.com>
2026-05-28 09:35:09 -0300

    a

--- Assets/Scriptys/FlashLight.cs
+++ Assets/Scriptys/FlashLight.cs
@@ -1,41 +1,99 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
+
+public enum ActiveState
+{
+    OFF, ON
+}
 
 public class FlashLight : MonoBehaviour
 {
-     private Light _light;
-     private float _originalIntensity;
+    private ActiveState _activeState = ActiveState.ON;
+    private Light _light;
+    private float _originalIntensity;
     [SerializeField] private float _intensityDecreaseRate = 0.5f;
     [SerializeField] private float _betteryDuration = 10;
     private bool _lostingPower;//Boleana que habilita a perda de intensidade da luz, para que a lanterna não perca intensidade antes do tempo determinado
+    private bool _isFullBattery = true;
+    private float _batteryTimer; //Timer para controlar o tempo de duração da bateria
 
     void Start()
     {
         _light = GetComponent<Light>();
         _originalIntensity = _light.intensity;
         GameController.Instance.OnUseBattery.AddListener(Recharge);
+        GameController.Instance.OnUseFlashlight.AddListener(TurnFlashlight);
+        _batteryTimer = _betteryDuration;
     }
 
     private void Recharge()
     {
         _light.intensity = _originalIntensity;
+        _batteryTimer = _betteryDuration;
         _lostingPower = false;
-        StopAllCoroutines();//Se o player usar uma pilha antes da antiga acabar, a contagem de tempo resetará
-        StartCoroutine(FullBattery());//Inicia a contagem de tempo para a bateria acabar
     }
 
-    IEnumerator FullBattery()
+
+    void Update()
     {
-        yield return new WaitForSeconds(_betteryDuration); //Tempo em que a lanterna não perde intensidade de luz
-        _lostingPower = true;
+        switch (_activeState) 
+        {   
+            case ActiveState.OFF:
+                //Se a lanterna estiver desligada, não precisa executar nada
+                //Os sistemas relacionandos a bateria ficam "Suspensos"
+                break;
+            case ActiveState.ON:
+                if (_lostingPower) //Aqui é caso a lanterna esteja perdendo a luz já
+                {
+                    if (_light.intensity <= 0) //Nullcheck para evitar que a intensidade da luz fique negativa
+                        return;
+                    _light.intensity -= Time.deltaTime * _intensityDecreaseRate;
+                }
+                else //Aqui é caso a lanterna esteja com bateria ainda boa
+                {
+                    _batteryTimer -= Time.deltaTime; //Diminui o timer da bateria a cada frame
+                    if (_batteryTimer <= 0)//Se o timer chegar acabar
+                    {
+                        _lostingPower = true; //Começa a perder intensidade
+                    }
+                }
+                     break;
+            default:
+                 break;
+        }
+
+    
     }
 
-     void Update()
-     {
-        if (!_lostingPower) //Se não estiver perdendo energia, não faça nada
-            return;
-        if (_light.intensity <= 0) //Nullcheck para evitar que a intensidade da luz fique negativa
-            return;
-       _light.intensity -= Time.deltaTime * _intensityDecreaseRate;
-     }
+    public void TurnFlashlight()
+    {
+        if (_activeState.Equals(ActiveState.ON))
+        {
+            SetState(ActiveState.OFF);
+        }
+        else 
+        {
+            SetState(ActiveState.ON);
+        }
+        
+    }
+
+    public void SetState(ActiveState newState)
+    {
+        switch (newState) 
+        {   
+            case ActiveState.OFF:
+                _light.enabled = false;
+                break;
+            case ActiveState.ON: 
+                _light.enabled = true;
+                break;
+            default:
+                 break;
+        }
+        _activeState = newState;
+    }
+
 }
--- Assets/Scriptys/Inventory.cs
+++ Assets/Scriptys/Inventory.cs
@@ -63,6 +63,9 @@ public class Inventory : MonoBehaviour
         _batteries--; //Começa com uma pilha
         GameController.Instance.OnUseBattery.Invoke();
     }
-
+    public void OnUseFlashlight(InputValue value)
+    {
+        GameController.Instance.OnUseFlashlight.Invoke();
+    }
 }
 
