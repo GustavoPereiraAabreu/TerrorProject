d82e4dac974b521db3c43dad28862ebbabd4499a
GUSTAVO PEREIRA ABREU <gustavop.4breu@gmail.com>
2026-06-10 09:27:59 -0300

    Ajuste Código

--- Assets/Scriptys/PatrolController.cs
+++ Assets/Scriptys/PatrolController.cs
@@ -3,10 +3,39 @@ using UnityEngine;
 public class PatrolController : MonoBehaviour
 {
     [SerializeField] private Transform[] _patrolPoints;
+    private int _currentPatrolIndex; //Ponto atual da patrulha
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
+    public Vector3 GetRandomPoint()
+    {
+        int randomIndex = Random.Range(0, _patrolPoints.Length);
+        return _patrolPoints[randomIndex].position;
+        
+    }
+    public Vector3 MoveToNextPoint()
+    { 
+      if (_patrolPoints.Length == 0)
+            return Vector3.zero;
+        Vector3 nextPoint = _patrolPoints[_currentPatrolIndex].position;
+        _currentPatrolIndex++;
+        if (_currentPatrolIndex >= _patrolPoints.Length)
+            _currentPatrolIndex = 0; // Volta para o primeiro ponto
+            return nextPoint;
+
+        /*
+         Se fosse fazer o mecanismo de booster do pokemon tcg
+         para montar um esquema de "carrossel", teria que adicionar uma verificação de valor mínimo
+         if (_currentPatrolIndex < 0)
+            _currentPatrolIndex = _patrolPoints.Length - 1;
+         */
+    }
+
+    //Criar método para retornar o ponto de patrulha mais próximo do inimigo
+    //Criar método para retornar o ponto de patrulha aleatório, mas que seja diferente do ponto atual do inimigo
 
 }
+
